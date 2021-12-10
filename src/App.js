4034222e035ee3773378ebe2d38cfaa27b010010
niroshkumar072 <niroshkumar072@.com>
2021-12-11 04:55:10 +0530

    start and stop functionality done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 const padTime = (time) => {
   return time.toString().padStart(2, "0");
 };
+
 export default function App() {
   const [title, setTitle] = useState("Start your 25 minute Goal now");
 
   const [timeleft, settimeLeft] = useState(25 * 60);
 
+  const [isRunning, setIsrunning] = useState(false);
+  const interval = useRef(null);
+
   const startTime = () => {
-    setInterval(() => {
+    if (interval.current !== null) return;
+    setTitle("Go Get'em🔥👀⏳");
+
+    setIsrunning(true);
+    interval.current = setInterval(() => {
       settimeLeft((timeleft) => {
-        setTitle("Got Get it");
         if (timeleft >= 1) return timeleft - 1;
+        resettimer();
         return 0;
       });
     }, 1000);
+  };
+  const stopTimer = () => {
+    if (interval.current === null) return;
+    clearInterval(interval.current);
+    if (minutes > 20) {
+      setTitle("Come on, don't give up yet 🐣");
+    } else setTitle("Good job👏💯");
+    interval.current = null;
+    setIsrunning(false);
+  };
+
+  const resettimer = () => {
+    clearInterval(interval.current);
+    setTitle("Hit it again ➿");
+    settimeLeft(25 * 60);
+    interval.current = null;
+    setIsrunning(false);
   };
 
   const minutes = padTime(Math.floor(timeleft / 60));
@@ -37,9 +62,9 @@ export default function App() {
         </div>
       </div>
       <div className="buttons">
-        <button onClick={() => startTime()}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTime}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resettimer}>Reset</button>
       </div>
     </div>
   );
